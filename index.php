13166ffe7fbd51e0c65ef03dad2030db87669d73
unknown <bbuisson@gamingcampus.local>
2024-11-28 11:32:54 +0100

    db update 2

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+require_once(__DIR__ . '/config/mysql.php');
+require_once(__DIR__ . '/databaseconnect.php');
+require_once(__DIR__ . '/variables.php');
+?>
 <!DOCTYPE html>
 <html>
 
--- sql/creation_base.sql
+++ sql/creation_base.sql
@@ -1,46 +1,37 @@
-CREATE DATABASE site_web;
+CREATE TABLE Entreprises(
+    ID int AUTO_INCREMENT PRIMARY KEY,
+    Entreprise varchar(255),
+    Projet varchar(255)
+);
 
-CREATE TABLE 'Entreprises'(
-    ID int,
-    Entreprise varchar,
-    Projet varchar
-)
+INSERT INTO Entreprises (ID, Entreprise, Projet) 
+VALUES 
+    (1, 'Royal Caribbean Int', 'Restaurant Flottant Autonome'),
+    (2, 'Ecoventura', 'Hôtel Flottant Écologique'),
+    (3, 'The Walt Disney Company', 'Bibliothèque du Futur'),
+    (4, 'Tesla', 'Eco-Village Intelligent'),
+    (5, 'SpaceX', 'Centre Spatial Intéractif');
 
-INSERT INTO 'Entreprises'(
-    VALUES(
-        ("1","Royal Caribbean Int","Restaurant Flottant Autonome"),
-        ("2","Ecoventura","Hôtel Flottant Écologique"),
-        ("3","The Walt Disney Company","Bibliothèque du Futur"),
-        ("4","Tesla","Eco-Village Intelligent"),
-        ("5","SpaceX","Centre Spatial Intéractif")
-    )
-)
+CREATE TABLE Skills(
+    ID int AUTO_INCREMENT PRIMARY KEY,
+    Skills varchar(255)
+);
 
-CREATE TABLE 'Skills'(
-    ID int,
-    Skills varchar
-)
+INSERT INTO Skills (Skills) 
+VALUES 
+    ('Full Stack Developer'),
+    ('Machine Learning'),
+    ('Cloud Computing'),
+    ('C/C++ Developer'),
+    ('API Design et Développement');
 
-INSERT INTO 'Skills'(
-    VALUES(
-        ("1","Full Stack developper"),
-        ("2","Machine Learning"),
-        ("3","Cloud Computing"),
-        ("4","C/C++ Developer"),
-        ("5","API Design et Developpement"),
-    )
-)
-
-CREATE TABLE 'info'(
-    ID int,
-    First_name varchar,
-    Last_name varchar,
+CREATE TABLE info(
+    ID int AUTO_INCREMENT PRIMARY KEY,
+    First_name varchar(255),
+    Last_name varchar(255),
     Age int,
-    Diploma varchar
-)
+    Diploma varchar(255)
+);
 
-INSERT INTO 'info'(
-    VALUES(
-        ("1","Natasha","Garcia","28","Master expert en ingénierie et développement d'applications au MIT")
-    )
-)
+INSERT INTO info (First_name, Last_name, Age, Diploma) 
+VALUES ('Natasha', 'Garcia', 28, 'Master expert en ingénierie et développement d\'applications au MIT');
